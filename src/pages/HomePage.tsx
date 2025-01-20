24e472061c64894dddff7f19f9281e13ef214953
Javier Lona <javierlona1@gmail.com>
2025-01-20 15:15:04 -0600

    Conditional rendering

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
         subtitle="Find the React job that fits your skills set"
       />
       <HomeCards />
-      <JobListings />
+      <JobListings isHome={true} />
       <ViewAllJobs />
     </>
   );
