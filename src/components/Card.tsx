6a3635b6cdc30076bcd7bfa5879666e371ac4801
Javier Lona <javierlona1@gmail.com>
2025-01-19 20:17:28 -0600

    Updating component state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-interface CardProps {
+type CardProps = {
   children: React.ReactNode;
   bg?: string;
-}
+};
 
 const Card: React.FC<CardProps> = ({ children, bg = "bg-gray-100" }) => {
   return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -10,6 +10,14 @@ type Job = {
 };
 
 const JobListing: React.FC<{ job: Job }> = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = React.useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 100) + "...";
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -18,7 +26,14 @@ const JobListing: React.FC<{ job: Job }> = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Show less" : "Show more"}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
