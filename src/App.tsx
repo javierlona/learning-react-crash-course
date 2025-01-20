0510ed57fdc4dcfc32279fa1d39a8d208ba2bd1e
Javier Lona <javierlona1@gmail.com>
2025-01-20 13:56:13 -0600

    Create HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero
-        title="Become a React Dev"
-        subtitle="Find the React job that fits your skills set"
-      />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills set"
+      />
+    </>
+  );
+};
+
+export default HomePage;
