1a99bde2917c76313bf5307ef1665aecc68a14fe
Javier Lona <javierlona1@gmail.com>
2025-01-20 15:56:22 -0600

    Utilize JSON Server

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import JobListing from "./JobListing";
-import jobs from "../jobs.json";
+
+type Job = {
+  id: string;
+  type: string;
+  title: string;
+  description: string;
+  salary: string;
+  location: string;
+};
 
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/jobs");
+        const data = await response.json();
+        setJobs(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -11,7 +37,7 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Featured Jobs" : "All Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
+          {jobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
